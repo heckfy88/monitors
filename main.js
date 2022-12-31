cd3643c253ae63eecb74c0ca579fdcf4e83280e4
Ruslan Agaev <ruslanagaev@list.ru>
2022-12-31 15:17:11 +0200

    #1 - инициализация проекта, проблема с бегущей строкой

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.marquee').marquee({
+        allowCss3Support: true,
+        css3easing: 'linear',
+        easing: 'linear',
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: false,
+        duration: 10000,
+        gap: 1,
+        pauseOnCycle: false,
+        pauseOnHover: false,
+        startVisible: false,
+        repeat: true
+    });
+});
